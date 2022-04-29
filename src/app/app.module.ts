f42c8d6a3aa63ceaad6ad3cbbb6ab7a37dc56f77
Krishna <onlinehelpbook@gmail.com>
2022-04-29 11:43:01 +0530

    Making First Web Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
